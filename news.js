e65384b1ec2dd71cba7e8b5664cbfe46cf7abeae
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-09-03 14:20:08 +0600

    dynamic view added

--- news.js
+++ news.js
@@ -43,10 +43,35 @@ const displayNews = (news) => {
     <div class="card p-4">
             <img src="${data.thumbnail_url}" class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">${data.title}</h5>
+              <h5 class="card-title">${data.title.slice(0, 50)}...</h5>
               <p class="card-text">
                 ${data.details.slice(0, 200)} ...
               </p>
+              <div class="d-flex align-items-center mb-3">
+                <img
+                  src= "${data.author.img}"
+                  class="rounded-circle w-25 me-2"
+                  alt="..."
+                />
+                <div>
+                  <h6>${data.author.name}</h6>
+                  <h6 class="text-muted">${data.author.published_date}</h6>
+                </div>
+              </div>
+              <div class="d-flex justify-content-between">
+                <div>
+                  <i class="fa-solid fa-eye text-muted"></i>
+                  <p class="d-inline ms-2">${data.total_view}</p>
+                </div>
+                <div>
+                  <i class="fa-solid fa-star-half-stroke"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+              </div>
+              <button class ="btn btn-primary mt-2"> Details </button>
             </div>
           </div>
     `;
